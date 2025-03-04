bc943a3a47c92ac6569dbb9b3e94d9c4d997db5f
Natzaum2 <103574@aluno.uricer.edu.br>
2025-03-04 16:19:49 -0300

    feat: adding login route and method

--- Backend/src/controllers/UserController.ts
+++ Backend/src/controllers/UserController.ts
@@ -2,8 +2,12 @@ import { Request, Response } from "express";
 import { AppDataSource } from "../config/database";
 import { User } from "../models/User";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export class UserController {
+
+    // ------------- Register --------------- //
+
     public static async register(req: Request, res: Response): Promise<Response> {
         try {
             const { name, email, password } = req.body;
@@ -22,6 +26,37 @@ export class UserController {
 
             return res.status(201).json({ message: "User created successfully", user: newUser });
         } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Something went wrong" });
+        }
+    }
+
+    // ------------- Login --------------- //
+
+    public static async login(req: Request, res: Response): Promise<Response> {
+        try {
+            const { email, password } = req.body;
+
+            const userRepository = AppDataSource.getRepository(User);
+            const user = await userRepository.findOne({ where: { email } });
+            if (!user) {
+                return res.status(401).json({ message: "Invalid email or password" });
+            }
+
+            const passwordMatch = await bcrypt.compare(password, user.password);
+            if(!passwordMatch) {
+                return res.status(401).json({ message: "Invalid email or password" });
+            }
+
+            const token = jwt.sign(
+                { id: user.id, email: user.email },
+                process.env.JWT_SECRET || "default_secret",
+                { expiresIn: "1h" }
+            );
+
+            return res.status(200).json({ message: "Login sucessful", token });
+
+        } catch (error){
             console.error(error);
             return res.status(500).json({ message: "Something went wrong" });
         }
