aa7599ef9f6e7e61ba5fee114994fe2fe2ce66c3
Natzaum2 <103574@aluno.uricer.edu.br>
2025-03-14 18:43:09 -0300

    feat: Product PUT

--- Backend/src/controllers/ProductController.ts
+++ Backend/src/controllers/ProductController.ts
@@ -3,6 +3,9 @@ import { AppDataSource} from "../config/database";
 import { Product } from "../models/Product";
 
 export class ProductController {
+
+    // ------------- Register --------------- //
+
     public static async registerProduct(req: Request, res: Response): Promise<Response> {
         try {
             const { name, description, price, imageUrl, category, available } = req.body;
@@ -16,6 +19,38 @@ export class ProductController {
 
         } catch (error) {
             console.error(error);
+            return res.status(500).json({ message: "Something went wrong" });
+        }
+    }
+
+    // ------------- Edit --------------- //
+
+    public static async editProduct(req: Request, res: Response): Promise<Response> {
+        try {
+            const { id } = req.params;
+            const { name, description, price, imageUrl, category, available } = req.body;
+
+            const productRepository = AppDataSource.getRepository(Product);
+
+            const product = await productRepository.findOne({ where: { id: Number(id) } });
+
+            if(!product) {
+                return res.status(404).json({ message: "Product not found" });
+            }
+
+            if (name) product.name = name;
+            if (description) product.description = description;
+            if (price) product.price = price;
+            if (imageUrl) product.imageUrl = imageUrl;
+            if (category) product.category = category;
+            if (available !== undefined) product.available = available;
+
+            await productRepository.save(product);
+
+            return res.status(201).json({ message: "Product edited successfully", product});
+
+        } catch (e) {
+            console.log(e);
             return res.status(500).json({ message: "Something went wrong" });
         }
     }
